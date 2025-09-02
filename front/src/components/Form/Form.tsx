71c12ee17ffd2e6f714bb306a37b4b7ab6c66b71
Alexey917 <pochtovii.1996@mail.ru>
2025-09-02 20:59:39 +0300

    фикс

--- front/src/components/Form/Form.tsx
+++ front/src/components/Form/Form.tsx
@@ -35,7 +35,7 @@ const Form = () => {
     return () => {
       tg.offEvent('mainButtonClicked', onSendData)
     }
-  }, [])
+  }, [onSendData])
 
   useEffect(() => {
     tg.MainButton.setParams({
