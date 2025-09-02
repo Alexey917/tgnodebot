da10c9cdbf5aaac28366c3a61e15ebda452cb56e
Alexey917 <pochtovii.1996@mail.ru>
2025-09-02 19:20:40 +0300

    добавил форму с управляемыми инпутами

--- front/src/components/Form/Form.tsx
+++ front/src/components/Form/Form.tsx
@@ -1,11 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useTelegram } from "../../hooks/useTelegram"
 
 const Form = () => {
+  const [country, setCountry] = useState<string>('');
+  const [street, setStreet] = useState<string>('');
+  const [subject, setSubject] = useState<string>('physical');
   const { tg } = useTelegram();
 
+  const changeCountry = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCountry(e.target.value)
+  }
+
+  const changeStreet = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStreet(e.target.value)
+  }
+
+  const changeSubject = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSubject(e.target.value)
+  }
+
   useEffect(() => {
-    tg.MainButton.setParams({text: 'Отправить данные', color: 'aqua'})
+    tg.MainButton.setParams({text: 'Отправить данные'})
   }, [])
 
 
@@ -13,9 +28,12 @@ const Form = () => {
     <form>
       <legend>Форма</legend>
       <fieldset>
-        <input type="text" />
-        <input type="text" />
-        <select name="" id=""></select>
+        <input type="text" placeholder="Страна" value={country} onChange={changeCountry}/>
+        <input type="text" placeholder="Улица" value={street}  onChange={changeStreet} />
+        <select value={subject} onChange={changeSubject}>
+          <option value={'physical'}>Физ. лицо</option>
+          <option value={'legal'}>Юр. лицо</option>
+        </select>
       </fieldset>
     </form>
   )
